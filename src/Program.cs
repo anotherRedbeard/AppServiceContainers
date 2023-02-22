3721803204d73c00035362aec40452f3a66df075
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2023-02-22 14:36:35 -0600

    add actual code

---
+++ src/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var corsAllowedHosts = builder.Configuration["CorsAllowedHosts"].Split(',');
+
+//policy.WithOrigins("https://localhost:7270","http://localhost:5118","https://win-wire-app--8nfao0p.redgrass-633dc5ff.eastus.azurecontainerapps.io")
+app.UseCors(policy => policy
+    .WithOrigins(corsAllowedHosts)
+    .AllowAnyMethod()
+    .WithHeaders(Microsoft.Net.Http.Headers.HeaderNames.ContentType)
+);
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
